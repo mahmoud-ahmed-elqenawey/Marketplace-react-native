1c1e7ee4b18f8e80cb136ea5425ad26ace1c7d92
QeNo <moodiiy.98@gmail.com>
2022-08-25 22:14:40 +0200

    add app components

---
+++ components/Subinfo.js
@@ -0,0 +1,59 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import { SIZES } from '../constants';
+
+export const NFTTitle = () => {
+    return (
+        <View>
+            <Text>NFTTitle</Text>
+        </View>
+    );
+};
+
+export const EthPrice = () => {
+    return (
+        <View>
+            <Text>EthPrice</Text>
+        </View>
+    );
+};
+
+export const ImageCmp = () => {
+    return (
+        <View>
+            <Text>ImageCmp</Text>
+        </View>
+    );
+};
+
+export const People = () => {
+    return (
+        <View>
+            <Text>People</Text>
+        </View>
+    );
+};
+
+export const EndDate = () => {
+    return (
+        <View>
+            <Text>EndDate</Text>
+        </View>
+    );
+};
+
+export const Subinfo = () => {
+    return (
+        <View
+            style={{
+                width: '100%',
+                paddingHorizontal: SIZES.font,
+                marginTop: -SIZES.extraLarge,
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+            }}
+        >
+            <Text>Subinfo</Text>
+        </View>
+    );
+};
---
+++ components/index.js
@@ -0,0 +1,6 @@
+import HomeHeader from './HomeHeader';
+import FocusedStatusBar from './FocusedStatusBar';
+import NEFTCard from './NEFTCard';
+import { CircleButton, RectButton } from './Button';
+
+export { HomeHeader, FocusedStatusBar, NEFTCard, CircleButton, RectButton };
